60682887e0c3ec8bce3d469f175dd58076fdae63
Adrien-Dorise <adrien.dorise@hotmail.com>
2024-01-05 23:49:24 +0100

    Added a discrete ball agent for the first example

---
+++ Assets/Scripts/First_example/rollerAgentDiscrete.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class rollerAgentDiscrete : Agent
+{
+    private Rigidbody rBody;
+    void Start () 
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            discreteActionsOut[0] = 0;
+        }
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            discreteActionsOut[0] = 2;
+        }
+        else if(Input.GetAxis("Vertical") > 0)
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            discreteActionsOut[0] = 4;
+        }
+    }
+
+    public Transform Target;
+    public override void OnEpisodeBegin()
+    {
+       // If the Agent fell, zero its momentum
+        if (this.transform.localPosition.y < 0)
+        {
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3( 0, 0.5f, 0);
+        }
+
+        // Move the target to a new spot
+        Target.localPosition = new Vector3(Random.value * 8 - 4,
+                                           0.5f,
+                                           Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+
+        if(actionBuffers.DiscreteActions[0] == 0)
+        {
+            controlSignal.x = 0;
+            controlSignal.z = 0;
+        }
+        else if(actionBuffers.DiscreteActions[0] == 1)
+        {
+            controlSignal.x = 1;
+            controlSignal.z = 0;
+        }
+        else if(actionBuffers.DiscreteActions[0] == 2)
+        {
+            controlSignal.x = -1;
+            controlSignal.z = 0;
+        }
+
+        if(actionBuffers.DiscreteActions[0] == 3)
+        {
+            controlSignal.x = 0;
+            controlSignal.z = 1;
+        }
+        else if(actionBuffers.DiscreteActions[0] == 4)
+        {
+            controlSignal.x = 0;
+            controlSignal.z = -1;
+        }
+
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+}
